04c33700fc512a895f7873289d9b1869911a6dc6
Scott Morrison <feh@qlik.com>
2019-10-08 14:04:57 -0400

    initial commit of country-locale-map

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs'); 
+function CLM() {
+    var clm;
+    var rawdata = fs.readFileSync('countries.json');
+    var countries = JSON.parse(rawdata);
+
+    var countryByAlpha2Code = {};
+
+    for(let i=0; i<countries.length; ++i) {
+        countryByAlpha2Code[countries[i]['alpha2']] = countries[i];
+    }
+
+    clm.getAlpha3ByAlpha2 = function(alpha2) {
+        return countryByAlpha2Code[alpha2].alpha3;
+    }; 
+
+    clm.getLocaleByAlpha2 = function(alpha2) {
+        return countryByAlpha2Code[alpha2].default_local;
+    }
+
+    return clm; 
+
+}
+
+module.export = CLM()
